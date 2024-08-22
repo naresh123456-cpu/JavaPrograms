4d152be0333b2214adae536b866aa6606596b8d5
harikrishna <harikrishna.g@motivitylabs.com>
2024-08-23 00:35:18 +0530

    updated code

--- src/test/java/ArraysProgramms/FindThDuplicateElementsInArray.java
+++ src/test/java/ArraysProgramms/FindThDuplicateElementsInArray.java
@@ -4,17 +4,17 @@ public class FindThDuplicateElementsInArray {
 
 	public static void main(String[] args) {
 		int a[]= {1,4,5,6,1,7};// compare first value with rest all values
-		boolean flag=false;
+		int count=0;
 		for(int i=0;i<a.length-1;i++) {
 			
 			for (int j=i+1;j<a.length;j++) {
 				if(a[i]==a[j]) {
 
 					System.out.println("Found Duplicate value : "+a[i]);
-					flag=true;
+					count++;
 				}
 			}
-		}if(flag==false) {
+		}if(count==0) {
 		System.out.println("Duplicate elements not found");
 	}
 	}
